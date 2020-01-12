d4b9050e3042e4f0ec3f65279fd9553f349e6cac
gwhyte96 <grahamwhyte25@gmail.com>
2020-01-12 03:25:21 -0800

    Adding text sending/recieving on app

--- nlp-scripts/app.py
+++ nlp-scripts/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import request, redirect, render_template
 # -*- coding: utf-8 -*-
-
+from twilio.rest import Client
 import support.fun0
 
 
@@ -26,6 +26,38 @@ def index():
     ############################
 
     return render_template("index.html", output=output)
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_receive():
+    # Display sent message
+    number = request.form['From']
+    msg = request.form["Body"]
+    print("Message recieved from: " + number)
+    print("Message: " + msg)
+
+    result1 =  support.fun0.input_taker(msg,'sentiment')
+    
+    resFeeling = 'I am quite happy' 
+    
+    if result1 is not None and result1['score'] != '':
+        if float(result1['score']) >= 0.75: 
+            resFeeling = 'I am quite happy' 
+        elif float(result1['score'])  >= 0.5: 
+            resFeeling = 'I am okay'
+        elif float(result1['score'])  >= 0.25: 
+            resFeeling = 'I am not okay'
+        else: 
+            resFeeling = 'I am sad'   
+    else:
+        result1 = {'doc':'','language':'','score':'','name':''}
+        result1['score'] = 'Please enter text'
+        resFeeling = 'Invalid'
+
+
+    print(resFeeling)
+    output = result1['score'] 
+
+    return render_template("index.html", ana = output)
 
 
 if __name__ == __name__:
